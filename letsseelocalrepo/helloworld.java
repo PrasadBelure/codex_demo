e88da1d38e6a06362b48a867bc9c27bed3aa00f2
Prasad Belure <prasadbelure07@gmail.com>
2023-09-19 23:20:27 +0530

    why is this possible

--- letsseelocalrepo/helloworld.java
+++
@@ -1,9 +0,0 @@
-package codex_demo.letsseelocalrepo;
-class helloworld{
-
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("whatsup people");
-    }
-
-}
