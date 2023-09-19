fb41c6cd09d3729e9b51951eae7b6a0065c8fdc6
Prasad Belure <prasadbelure07@gmail.com>
2023-09-19 16:56:39 +0530

    lets do it

---
+++ letsseelocalrepo/helloworld.java
@@ -0,0 +1,9 @@
+package codex_demo.letsseelocalrepo;
+class helloworld{
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("whatsup people");
+    }
+
+}
