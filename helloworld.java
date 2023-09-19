2d8ff2b61a02e408f380cc0f018ed15ee35e5320
Prasad Belure <prasadbelure07@gmail.com>
2023-09-19 23:35:04 +0530

    i tried cide editior

--- helloworld.java
+++ helloworld.java
@@ -3,10 +3,9 @@ class helloworld{
 
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("whatsup people");
-        System.out.println("this is a new feature ");
-        lets try to know that
-        hey people lets go to talk to person 
+        System.out.println("whatsup people");    
+        System.out.println("hey what are you doing ??");
+        System.out.println("lets go add new commit ");
     }
 
 }
